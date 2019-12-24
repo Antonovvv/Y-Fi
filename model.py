d386464731c3eeb91cb9a01a47486c17197cb56d
Antonovvv <1228618646@qq.com>
2019-12-25 02:39:34 +0800

    1、视角缩放，平移事件 2、基本仿真模型搭建 3、透明度显示问题解决

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import numpy as np
 class GLWindow:
 
     IS_PERSPECTIVE = True   # 透视投影
-    VIEW = np.array([-1.0, 1.0, -1.0, 1.0, 1.0, 100.0])  # 视景体的left/right/bottom/top/near/far六个面
+    VIEW = np.array([-0.8, 0.8, -0.8, 0.8, 1.0, 1000.0])  # 视景体的left/right/bottom/top/near/far六个面
     SCALE_K = np.array([1.0, 1.0, 1.0])     # 模型缩放比例
     EYE = np.array([0.0, 0.0, 4.0])         # 眼睛的位置（默认z轴的正方向）
     LOOK_AT = np.array([0.0, 0.0, 0.0])     # 瞄准方向的参考点（默认在坐标原点）
@@ -16,12 +16,14 @@ class GLWindow:
     WIN_W, WIN_H = 640, 480                 # 保存窗口宽度和高度的变量
     LEFT_IS_DOWNED = False                  # 鼠标左键被按下
     MOUSE_X, MOUSE_Y = 0, 0                 # 考察鼠标位移量时保存的起始位置
+    dx, dy, dz = 0, 0, 0                    # 平移偏移量
 
     def __init__(self):
         self.DIST, self.PHI, self.THETA = self.getposture()  # 眼睛与观察目标之间的距离、仰角、方位角
 
     def init(self):
-        glClearColor(1.0, 1.0, 1.0, 1.0)
+        glClearColor(0.4, 0.8, 1.0, 1.0)
+
         glEnable(GL_DEPTH_TEST)     # 开启深度测试，实现遮挡关系
         glDepthFunc(GL_LEQUAL)      # 设置深度测试函数,GL_LEQUAL只是选项之一
 
@@ -37,6 +39,9 @@ class GLWindow:
         return dist, phi, theta
 
     def draw(self):
+        # 设置透明显示
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         # 清除屏幕及深度缓存
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -57,7 +62,7 @@ class GLWindow:
                 glFrustum(view[0], view[1], view[2] * height / width, view[3] * height / width, view[4], view[5])
             else:
                 glOrtho(view[0], view[1], view[2] * height / width, view[3] * height / width, view[4], view[5])
-        '''
+
         # 设置模型视图
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
@@ -65,7 +70,7 @@ class GLWindow:
         # 几何变换
         scale = self.SCALE_K
         glScale(scale[0], scale[1], scale[2])
-        '''
+
         # 设置视点
         eye = self.EYE
         eye_up = self.EYE_UP
@@ -79,48 +84,60 @@ class GLWindow:
         # 设置视口
         glViewport(0, 0, width, height)
 
-        # -----------------------------------------
+        # 平移
+        glTranslatef(self.dx, self.dy, self.dz)
+
         # 绘制坐标系
+        '''
         xyz_depth = 2.0
         glBegin(GL_LINES)
         glColor4f(0.0, 0.0, 0.0, 1.0)
         glVertex3f(-xyz_depth, 0.0, 0.0)
         glVertex3f(xyz_depth, 0.0, 0.0)
-
         glColor4f(0.0, 0.0, 0.0, 1.0)
         glVertex3f(0.0, -xyz_depth, 0.0)
         glVertex3f(0.0, xyz_depth, 0.0)
-
         glColor4f(0.0, 0.0, 0.0, 1.0)
         glVertex3f(0.0, 0.0, -xyz_depth)
         glVertex3f(0.0, 0.0, xyz_depth)
+        glEnd()'''
+
+        sqare = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
+        room_length = 10.0
+        room_width = 7.0
+        land = 100.0
+        # 绘制后面
+        glBegin(GL_QUADS)
+        for i in range(4):
+            glColor4f(0.75, 0.75, 0.75, 1.0)
+            glVertex3f(sqare[i][0]*room_length, sqare[i][1]*5.0, -room_width)
         glEnd()
 
-        # 绘制后面
-        glBegin(GL_POLYGON)
-        glColor4f(0.4, 0.8, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(-1.0, -1.0, -1.0)    # 设置顶点
-        glColor4f(0.4, 0.8, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(1.0, -1.0, -1.0)     # 设置顶点
-        glColor4f(0.4, 0.8, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(1.0, 1.0, -1.0)      # 设置顶点
-        glColor4f(0.4, 0.8, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(-1.0, 1.0, -1.0)     # 设置顶点
+        # 绘制左侧、顶面、右侧
+        glBegin(GL_QUAD_STRIP)
+        for i in range(4):
+            glColor4f(0.75, 0.75, 0.75, 1.0)
+            glVertex3f(sqare[i][0] * room_length, sqare[i][1] * 5.0, room_width)
+            glColor4f(0.75, 0.75, 0.75, 1.0)
+            glVertex3f(sqare[i][0] * room_length, sqare[i][1] * 5.0, -room_width)
         glEnd()
 
         # 绘制底面
         glBegin(GL_POLYGON)
-        glColor4f(0.0, 0.0, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(-1.0, -1.0, -1.0)    # 设置顶点
-        glColor4f(0.0, 0.0, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(1.0, -1.0, -1.0)     # 设置顶点
-        glColor4f(0.0, 0.0, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(1.0, -1.0, 1.0)      # 设置顶点
-        glColor4f(0.0, 0.0, 1.0, 1.0)   # 设置当前颜色为__色不透明
-        glVertex3f(-1.0, -1.0, 1.0)     # 设置顶点
+        for i in range(4):
+            glColor4f(0.75, 0.56, 0.375, 1.0)
+            glVertex3f(sqare[i][0]*land, -5.0, sqare[i][1]*land)
         glEnd()
 
-        # ------------------------------------
+        # 绘制点
+        glDepthMask(GL_FALSE)
+        glPointSize(5.0)
+        glBegin(GL_POINTS)
+        glColor4f(1.0, 0.0, 0.0, 0.5)
+        glVertex3f(0.5, 0.5, 0.5)
+        glEnd()
+        glDepthMask(GL_TRUE)
+
         # 切换缓冲区，以显示绘制内容
         glutSwapBuffers()
 
@@ -173,19 +190,20 @@ class GLWindow:
             glutPostRedisplay()
 
     def keydown(self, key, x, y):
+        speed = 0.4
         if key in [b'a', b'd', b'y', b'Y', b'w', b's']:
-            if key == b'a':  # 瞄准参考点 x 减小
-                self.LOOK_AT[0] -= 0.01
-            elif key == b'd':  # 瞄准参考 x 增大
-                self.LOOK_AT[0] += 0.01
+            if key == b'd':  # 瞄准参考点 x 减小
+                self.dx -= speed
+            elif key == b'a':  # 瞄准参考 x 增大
+                self.dx += speed
             elif key == b'y':  # 瞄准参考点 y 减小
-                self.LOOK_AT[1] -= 0.01
+                self.LOOK_AT[1] -= speed
             elif key == b'Y':  # 瞄准参考点 y 增大
-                self.LOOK_AT[1] += 0.01
-            elif key == b'w':  # 瞄准参考点 z 减小
-                self.LOOK_AT[2] -= 0.01
-            elif key == b's':  # 瞄准参考点 z 增大
-                self.LOOK_AT[2] += 0.01
+                self.LOOK_AT[1] += speed
+            elif key == b's':  # 瞄准参考点 z 减小
+                self.dz -= speed
+            elif key == b'w':  # 瞄准参考点 z 增大
+                self.dz += speed
 
             self.DIST, self.PHI, self.THETA = self.getposture()
             glutPostRedisplay()
